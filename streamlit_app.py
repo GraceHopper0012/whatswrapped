bcd37c741129d669bbdb3bcc28c738facf267d82
GraceHopper0012 <145455023+GraceHopper0012@users.noreply.github.com>
2026-03-17 11:24:48 +0000

    basic upload functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+import sqlite3
+import os
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+UPLOAD_DIR = "upload"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+DB_PATH = os.path.join(UPLOAD_DIR, "messages.db")
+
+st.title("WhatsWrapped")
+
+uploaded = st.file_uploader("Your unencrypted Wahtsapp DB", type="db", accept_multiple_files=False)
+if uploaded is not None:
+    with open(db_path, "wb") as f:
+        f.write(uploaded.read())
+
+if os.path.exists(DB_PATH):
+    conn = sqlite3.connect(DB_PATH)
